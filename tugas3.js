702d7aa9543abe3212d0f150a81e1c857655ed70
User-Niomic-Fikri <fikrifauzan17@gmail.com>
2020-04-15 13:10:47 +0700

    Tugas 3 JavaScript
    tugas 3 js niaomic

---
+++ tugas3.js
@@ -0,0 +1,18 @@
+//sebelum dirubah
+function tugas3_2(x){
+
+console.log(x);
+
+  return x;
+}
+tugas3_2(100);
+//sesudah dirubah
+function tugas3_1(x){
+  var ubahdata = x;
+  ubahdata = 144;
+
+  return ubahdata;
+}
+tugas3_1(100);
+
+console.log(tugas3_1());
